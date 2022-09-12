712fe1b6b8f24549e1685a23dded7ee6b8598474
Gastón Fournier <gaston@getunleash.ai>
2022-09-12 12:44:58 +0200

    Attempt to move logic to GH action

--- run-tests.sh
+++ run-tests.sh
@@ -2,22 +2,12 @@
 set -e
 docker compose up -d postgres unleash-server initializer
 
-while [ "$(docker compose ps --format=json --status exited initializer)" == "null" ]; do
-    echo "Waiting initializer"
-    sleep 1
-done
+./server/wait-container-exit.sh initializer
 echo "Now server is setup"
 
 for i in 'node 3000' 'java 5100' 'python 5000'; do
     SDK=( $i )
-    docker compose up --build -d ${SDK[0]}
-    SDK_URL=http://localhost:${SDK[1]}
-    while ! curl -s -o /dev/null ${SDK_URL}; do
-	    echo "Waiting ${SDK_URL}"
-	    sleep 1
-    done
-    SDK_URL=${SDK_URL} yarn test
-    docker compose stop ${SDK[0]} &
+    ./server/run-test.sh ${SDK[0]} ${SDK[1]}
 done
 
 docker compose stop postgres unleash-server initializer
