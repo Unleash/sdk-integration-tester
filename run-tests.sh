e6e780630089bcebc0082c0a720b336f559a9bb2
Nuno Góis <github@nunogois.com>
2022-09-13 09:12:40 +0100

    update sh scripts to include SDK labels

--- run-tests.sh
+++ run-tests.sh
@@ -5,9 +5,9 @@ docker compose up -d postgres unleash-server initializer
 ./server/wait-container-exit.sh initializer
 echo "Now server is setup"
 
-for i in 'node 3000' 'java 5100' 'python 5001'; do
+for i in 'node 3000 NodeJS' 'java 5100 Java' 'python 5001 Python'; do
     SDK=( $i )
-    ./server/run-test.sh ${SDK[0]} ${SDK[1]}
+    ./server/run-test.sh ${SDK[0]} ${SDK[1]} ${SDK[2]}
 done
 
 docker compose stop postgres unleash-server initializer
