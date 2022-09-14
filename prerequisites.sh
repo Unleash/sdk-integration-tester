54a022b8f479a704be045f10ca4d65605ee99c93
Gastón Fournier <gaston@getunleash.ai>
2022-09-14 12:03:49 +0200

    Improve checkout of edge

--- prerequisites.sh
+++ prerequisites.sh
@@ -2,12 +2,15 @@
 set -e
 EDGE_BRANCH=${EDGE_BRANCH:-main}
 EDGE_FOLDER=${EDGE_FOLDER:-unleash-on-the-edge}
+CURRENT=`pwd`
 if [ ! -d ${EDGE_FOLDER} ]; then
-    git clone --depth 1 --branch ${EDGE_BRANCH} git@github.com:bricks-software/unleash-on-the-edge.git ${EDGE_FOLDER}
-else
-    CURRENT=`pwd`
+    git clone git@github.com:bricks-software/unleash-on-the-edge.git ${EDGE_FOLDER}
     cd ${EDGE_FOLDER}
     git checkout ${EDGE_BRANCH}
-    git pull origin ${EDGE_BRANCH}
-    cd ${CURRENT}
+else
+    cd ${EDGE_FOLDER}
+    git fetch
+    git checkout ${EDGE_BRANCH}
+    git merge --ff-only origin/${EDGE_BRANCH}
 fi
+cd ${CURRENT}
