01666c8ddf6edaa504af536c65b78958e0e536be
Gastón Fournier <gaston@getunleash.ai>
2022-09-14 12:26:58 +0200

    Ignore unleash-on-the-edge from jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testTimeout: 30 * 1000 // Set timeout to 30 seconds to allow heroku enough time to start up
+  testTimeout: 30 * 1000, // Set timeout to 30 seconds to allow heroku enough time to start up
+  testPathIgnorePatterns : [
+    "<rootDir>/unleash-on-the-edge" 
+  ]
 };
