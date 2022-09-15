1565e35b37f03ce20a162919257693a98fa8fdda
Gastón Fournier <gaston@getunleash.ai>
2022-09-15 11:38:30 +0200

    Update github action

--- server/wait-unleash-server-has-token.sh
+++ server/wait-unleash-server-has-token.sh
@@ -1,10 +1,10 @@
 while ! curl -s -o /dev/null \
     unleash-server:4242/health
-do echo "Waiting for unleash-server..." && sleep 1 
+do echo "Waiting for unleash-server health..." && sleep 1 
 done
 
 while ! curl -s -o /dev/null \
     -H 'Authorization: *:*.unleash-insecure-admin-api-token' \
     unleash-server:4242/api/admin/api-tokens
-do echo "Waiting for unleash-server..." && sleep 1 
+do echo "Waiting for unleash-server tokens..." && sleep 1 
 done
