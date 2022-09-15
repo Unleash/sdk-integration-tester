85d67e5fdd79e18de80feeb615e5a8b6d7ecb385
Gastón Fournier <gaston@getunleash.ai>
2022-09-15 15:13:51 +0200

    Fix listen to localhost

--- server/wait-unleash-server-has-token.sh
+++ server/wait-unleash-server-has-token.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 count=0
-while ! curl -s -o /dev/null \
-    unleash-server:4242/health
+while ! curl -s -o /dev/null localhost:4242/health
 do 
-    count=$((count + 1)) && if [ ${count} -gt 10 ]; then 
+    count=$((count + 1)) && if [ ${count} -gt 20 ]; then 
         echo "Failed to startup" 
         docker compose logs unleash-server
         exit 1; 
     fi
+    curl -s localhost:4242/health
     echo "Waiting for unleash-server health..." && sleep 1 
 done
 
 while ! curl -s -o /dev/null \
     -H 'Authorization: *:*.unleash-insecure-admin-api-token' \
-    unleash-server:4242/api/admin/api-tokens
+    localhost:4242/api/admin/api-tokens
 do 
-    count=$((count + 1)) && if [ ${count} -gt 10 ]; then 
+    count=$((count + 1)) && if [ ${count} -gt 20 ]; then 
         echo "Failed to startup" 
         docker compose logs unleash-server
         exit 1; 
