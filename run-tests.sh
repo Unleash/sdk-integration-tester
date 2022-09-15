a58d6122b1b04f9d5376cc4169c3bef921a527b2
Gastón Fournier <gaston@getunleash.ai>
2022-09-15 15:15:57 +0200

    Move helper test scripts to root

--- run-tests.sh
+++ run-tests.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
-docker compose up -d postgres unleash-server
-
-./server/wait-unleash-server-has-token.sh
-echo "Now server is setup"
 
 for i in 'node 3000 NodeJS' 'java 5100 Java' 'python 5001 Python'; do
+    echo 'Testing' $i
     SDK=( $i )
-    ./server/run-test.sh ${SDK[0]} ${SDK[1]} ${SDK[2]}
+    docker compose up -d postgres unleash-server
+
+    ./wait-unleash-server-has-token.sh || exit 1
+    echo "Now server is setup"
+
+    ./run-test.sh ${SDK[0]} ${SDK[1]} ${SDK[2]}
+    docker compose stop postgres unleash-server
+    docker compose rm -f postgres unleash-server
 done
 
-docker compose stop postgres unleash-server
