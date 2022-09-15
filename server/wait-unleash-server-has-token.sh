1bb1e46b99477979ea0624badcdd0e20460dfb53
Gastón Fournier <gaston@getunleash.ai>
2022-09-15 11:50:43 +0200

    Fail fast

--- server/wait-unleash-server-has-token.sh
+++ server/wait-unleash-server-has-token.sh
@@ -1,10 +1,24 @@
+#!/usr/bin/env bash
+count=0
 while ! curl -s -o /dev/null \
     unleash-server:4242/health
-do echo "Waiting for unleash-server health..." && sleep 1 
+do 
+    count=$((count + 1)) && if [ ${count} -gt 10 ]; then 
+        echo "Failed to startup" 
+        docker compose logs unleash-server
+        exit 1; 
+    fi
+    echo "Waiting for unleash-server health..." && sleep 1 
 done
 
 while ! curl -s -o /dev/null \
     -H 'Authorization: *:*.unleash-insecure-admin-api-token' \
     unleash-server:4242/api/admin/api-tokens
-do echo "Waiting for unleash-server tokens..." && sleep 1 
+do 
+    count=$((count + 1)) && if [ ${count} -gt 10 ]; then 
+        echo "Failed to startup" 
+        docker compose logs unleash-server
+        exit 1; 
+    fi
+    echo "Waiting for unleash-server tokens..." && sleep 1 
 done
