8b4a948d09c463c44cf056257ef6e10b94175bc1
Nuno Góis <github@nunogois.com>
2022-10-10 11:36:10 +0100

    upgrade simple.test.ts, add dotnet.yaml

--- src/simple.test.ts
+++ src/simple.test.ts
@@ -1,84 +1,140 @@
 import got from 'got'
 
-import { readdirSync, existsSync } from 'fs'
+import fs from 'fs'
+import { Network, StartedNetwork } from 'testcontainers'
 import {
-  Network,
-  StartedNetwork,
-} from 'testcontainers';
-import { TestConfiguration } from './lib/Config'
-import { ContainerInstance, UnleashServerInterface } from './lib/BaseContainers';
-const ADMIN_TOKEN = '*:*.unleash-insecure-admin-api-token'
+  MockServerConfig,
+  OSSServerConfig,
+  TestConfiguration,
+  TestYamlConfig
+} from './lib/Config'
+import { ContainerInstance, UnleashServerInterface } from './lib/BaseContainers'
+import YAML from 'yaml'
+import { SDKOptions } from './lib/SDKContainers'
 
-let config: TestConfiguration = {
-  postgres:{
-    image: 'postgres:alpine3.15',
-    dbName: 'unleash',
-    user: 'unleash_user',
-    password: 'unleash.the.password',
-  },
-  unleash: {
-    image: 'unleashorg/unleash-server:latest',
-    clientToken: '*:development.unleash-insecure-api-token',
-    adminToken: '*:*.unleash-insecure-admin-api-token',
+const rawConfig = fs.readFileSync(
+  `./src/${process.env.CONFIG || 'all-against-mock'}.yaml`,
+  'utf8'
+)
+const parsedConfig = YAML.parse(rawConfig) as TestYamlConfig
+
+const sdks = process.env.SDK
+  ? [{ name: process.env.SDK, type: process.env.SDK }]
+  : parsedConfig.sdks.map(c => {
+      c.name = c.name || c.type
+      return c
+    })
+
+const servers = process.env.SERVER
+  ? [{ type: process.env.SERVER }]
+  : parsedConfig.servers
+
+function parseConfig(
+  server: OSSServerConfig | MockServerConfig
+): TestConfiguration {
+  let config: TestConfiguration
+  if (server.type === 'OSSServer') {
+    const ossServer = server as OSSServerConfig
+    config = {
+      serverImpl: ossServer.type,
+      postgres: {
+        image: ossServer.postgresImage ?? 'postgres:alpine3.15',
+        dbName: 'unleash',
+        user: 'unleash_user',
+        password: 'unleash.the.password'
+      },
+      unleash: {
+        image: ossServer.image ?? 'unleashorg/unleash-server:latest',
+        clientToken: parsedConfig.clientToken,
+        adminToken: parsedConfig.adminToken
+      }
+    }
+  } else if (server.type === 'MockServer' || server.type === 'Edge') {
+    // This is pointless, we should remove TestConfiguration
+    config = {
+      serverImpl: server.type,
+      postgres: {
+        image: '',
+        dbName: '',
+        user: '',
+        password: ''
+      },
+      unleash: {
+        image: '',
+        clientToken: parsedConfig.clientToken,
+        adminToken: parsedConfig.adminToken
+      }
+    }
+  } else {
+    throw new Error(`Invalid server config type ${server.type}`)
   }
+  return config
 }
 
-const getDirectories = (source: string) =>
-  readdirSync(source, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .filter(dirent => existsSync(`${source}/${dirent.name}/container.ts`))
-    .map(dirent => dirent.name)
-    
-const sdks = process.env.SDK? [process.env.SDK] : getDirectories('src/sdks')
-describe("SDK tests", () => {
+describe.each(servers)(`$type`, server => {
   let unleashServer: ContainerInstance & UnleashServerInterface
   let network: StartedNetwork
+  let initialized = false
+  let sdkContainers = new Map<string, ContainerInstance>()
+  let config = parseConfig(server)
 
-  beforeEach(async () => {
-    try {
+  beforeAll(async () => {
+    if (!initialized) {
+      console.log(`===== Initializing Unleash ${server.type} =====`)
       network = await new Network().start()
       unleashServer = require('./servers/index').create(config, network)
       await unleashServer.initialize()
-    } catch (err) {
-      console.log(err)
+      initialized = true
+    } else {
+      console.log(`===== Reseting Unleash ${server.type} =====`)
+      await unleashServer.reset()
     }
-  });
 
-  async function setState(file: string) {
-    // TODO: this is a global state defined for all tests and reseted before each
-    // we need the state to be defined by the test either as a DB state or a json that can be imported
-    const state = require(file)
-    
-    await unleashServer.setState(state)
-  }
+    // ========= set unleash state (~50ms)
+    const state = require('./simple.test.state.json')
+    let succeed = await unleashServer.setState(state)
+    expect(succeed).toBeTruthy()
+  })
 
-  it.each(sdks)('SDK %p', async (sdk: string) => {
-    await setState('./simple.test.state.json')
+  describe.each(sdks)(`$name SDK`, sdkTestConfig => {
+    let sdkUrl: string
 
-    let options = {
-      unleashApiUrl: `http://${unleashServer.getInternalIpAddress()}:${unleashServer.getInternalPort()}/api`,
-      apiToken: ADMIN_TOKEN,
-      network: network
-    }
-    let { create } = require(`./sdks/${sdk}/container`)
-    let sdkContainer: ContainerInstance = create(options)
-    await sdkContainer.initialize()
-    const { body, statusCode } = await got.post(`http://localhost:${sdkContainer.getMappedPort()}/is-enabled`, {
-      headers: {
-        Authorization: ADMIN_TOKEN
-      },
-      json: {
-        toggle: 'test-on',
-        context: {
-          userId: '123'
+    beforeAll(async () => {
+      let sdkContainer = sdkContainers.get(sdkTestConfig.type)
+      if (!sdkContainer) {
+        // console.log(`===== Initializing ${sdk} =====`)
+        let options: SDKOptions = {
+          unleashApiUrl: `http://${unleashServer.getInternalIpAddress()}:${unleashServer.getInternalPort()}/api`,
+          apiToken: config.unleash.adminToken,
+          network: network,
+          sdkImpl: sdkTestConfig.client
         }
+        let { create } = require(`./sdks/${sdkTestConfig.type}/container`)
+        sdkContainer = create(options) as ContainerInstance
+        await sdkContainer.initialize()
+        sdkContainers.set(sdkTestConfig.name!, sdkContainer)
+      } else {
+        // cleanup cached SDK state
+        // console.log(`===== Reseting state of ${sdk} =====`)
+        await sdkContainer.reset()
       }
+      sdkUrl = `http://localhost:${sdkContainer.getMappedPort()}`
     })
-    console.log(body)
-    expect(statusCode).toBe(200)
-    let res = JSON.parse(body)
-    expect(res.name).toBe('test-on')
-    expect(res.enabled).toBeTruthy()
-    expect(res.context.userId).toBe('123')
-  });
-});
+
+    test('simple test', async () => {
+      const { body, statusCode } = await got.post(`${sdkUrl}/is-enabled`, {
+        json: {
+          toggle: 'test-on',
+          context: {
+            userId: '123'
+          }
+        }
+      })
+      expect(statusCode).toBe(200)
+      const result = JSON.parse(body)
+      expect(result.name).toBe('test-on')
+      expect(result.enabled).toBeTruthy()
+      expect(result.context.userId).toBe('123')
+    })
+  })
+})
